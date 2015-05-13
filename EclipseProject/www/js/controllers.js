0e123a7d2ca27150ba057f02268b70ec8050cf68
Marcos Acevedo <marcos@rushlock.com>
2015-05-13 11:28:00 -0400

    Create Eclipse Project structure for iOS deploy.

---
+++ EclipseProject/www/js/controllers.js
@@ -0,0 +1,6 @@
+angular.module('gestor.controllers', [])
+
+.controller('MainCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state){
+	alert("Im in main")
+	console.log("im in main");
+}]);
